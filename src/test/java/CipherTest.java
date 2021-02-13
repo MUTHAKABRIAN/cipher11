643dfbbc6eca2a10efa0a9d0ce8da9c857c401fa
MUTHAKABRIAN <muthakabrian@gmail.com>
2021-02-13 23:13:32 +0300

    test of creating an instance of Cipher Class

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -1,5 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CipherTest {
+    @Test
+    public void newCipher_instanceCorrectly() {
+        Cipher testCipher = new Cipher("a",2);
 
+    }
 }
