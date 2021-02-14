06e18d043e250b8cc935d8dd6570aed28e282293
MUTHAKABRIAN <muthakabrian@gmail.com>
2021-02-14 20:02:56 +0300

    add decryption method

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -20,7 +20,7 @@ public class Cipher {
         return statement;
     }
 
-    public static  String cipher( Cipher cipher) {
+    public static  String encrypt( Cipher cipher) {
         if (cipher.key> 26) {
             cipher.key = cipher.key % 26;
         }
@@ -57,5 +57,51 @@ public class Cipher {
             }
         }
         return outputString;
+    }
+    public static  String decrypt (Cipher cipher){
+        if(cipher.key >26) {
+            cipher.key = cipher.key % 26;
+        }
+        else if(cipher.key < 0){
+            cipher.key = (cipher.key % 26) + 26;
+
+        }
+        String outputString = "";
+        int length = cipher.statement.length();
+        for (int i = 0; i < length; i++){
+            char cc = cipher.statement.charAt(i);
+            if (Character.isLetter(cc)){
+                if (Character.isLowerCase(cc)){
+                    char c = (char)(cc - cipher.key);
+                    if (c>'z'){
+                        outputString += (char)(cc + (26-cipher.key));
+                    }
+
+                    else {
+                        outputString += c;
+
+                    }
+                }
+
+                else if (Character.isUpperCase(cc)){
+                    char c = (char)(cc - cipher.key);
+                    if (c>'Z'){
+                        outputString += (char)(cc + (26-cipher.key));
+                    }
+
+                    else {
+                        outputString += c;
+
+                    }
+                }
+
+
+            }
+            else {
+                outputString += cc;
+            }
+        }
+        return outputString;
+
     }
 }
