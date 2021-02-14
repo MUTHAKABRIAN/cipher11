f519fa57fbdae1f5aa53a4bc85d5b17819b1db09
MUTHAKABRIAN <muthakabrian@gmail.com>
2021-02-14 19:46:46 +0300

    create a test for encryption

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -5,21 +5,37 @@ import static org.junit.Assert.*;
 public class CipherTest {
     @Test
     public void newCipher_instanceCorrectly() {
-        Cipher testCipher = new Cipher("a",2);
+        Cipher testCipher = new Cipher("a", 2);
         assertEquals(true, testCipher instanceof Cipher);
 
     }
 
     @Test
     public void newCipher_getStatement_a() {
-        Cipher testCipher = new Cipher("a",2);
-        assertEquals("a",testCipher.getStatement());
+        Cipher testCipher = new Cipher("a", 2);
+        assertEquals("a", testCipher.getStatement());
 
     }
 
     @Test
     public void newCipher_getKey_2() {
-        Cipher testCipher =new Cipher("a",2);
-        assertEquals(2,testCipher.getKey());
+        Cipher testCipher = new Cipher("a", 2);
+        assertEquals(2, testCipher.getKey());
     }
+
+    @Test
+    public void newCipher_replaceLetter_b() {
+        Cipher testCipher = new Cipher("a", 2);
+        assertEquals("b", testCipher.isEncrypted());
+
+    }
+
+    @Test
+    public void runCipher_ifInputIsString() {
+        Cipher testCipher = new Cipher("abc", 3);
+        assertEquals("abc", testCipher.getStatement());
+    }
+
+
+
 }
